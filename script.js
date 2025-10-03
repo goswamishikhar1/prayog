62ace7c9be048bda3ec45019741e433d12529d05
goswamishikhar1 <ggoswamishikhar1@gmail.com>
2025-10-03 07:12:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function goToResults() {
     window.location.href = "results.html";
   }, 2000); // 2 second delay to show the loading animation
 }
+
+function checkResults() {
+  // Show loading screen
+  const loadingScreen = document.getElementById('loadingScreen');
+  loadingScreen.classList.add('show');
+  
+  // Redirect to detailed results
+  setTimeout(() => {
+    window.location.href = "detailed-results.html";
+  }, 2000);
+}
+
+function goBackHome() {
+  window.location.href = "home_page.html";
+}
+
+function goBackResults() {
+  window.location.href = "results.html";
+}
